bc46b5c0fb7e5cea5ef4ded8cd42cd7aea30233f
ronnie <rdcaldwell5705@eagle.fgcu.edu>
2016-06-20 17:21:12 +0000

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-'use strict'
-
-var express = require('express');
-var bodyParser = require("body-parser");
-
-var routes = require("./app/routes/index.js");
+var express = require("express");
+var moment = require("moment")
+var fs = require("fs");
+var path = require("path");
 var api = require("./app/app.js");
-
 var app = express();
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-var port = process.env.PORT;   
-
-routes(app);
-api(app);
+var port = process.env.PORT || 8080;   
 
 app.listen(port, function() {
   console.log("Node.js listening on port " + port);
 });
+
+app.get("/", function(req, res) {
+    var file = path.join(__dirname, "index.html")
+});
+
+app.get("/:query", function(req, res) {
+    var date = req.params.query;
+    var unix;
+    var natty;
+    var dateJSON = { "unix": unix, "natural": natty }
+        
+    if (+date === parseInt(date, 10)) {
+        unix = +date;
+        natty = unixToNatty(unix);
+    }
+    else if (moment(date, "MMMM D, YYYY").isValid()) {
+        natty = date;
+        unix = nattyToUnix(natty);
+    }
+    else {
+        unix = null;
+        natty = null;
+    }
+        
+    res.send(JSON.stringify(dateJSON));
+});
+    
+function unixToNatty(unix) {
+    return moment.unix(unix).format("MMMM D, YYYY");
+}
+    
+function nattyToUnix(natty) {
+    return natty.unix();
+}
