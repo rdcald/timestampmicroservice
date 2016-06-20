bc58747430e4f4e93f964965804cc2f0da087923
ronnie <rdcaldwell5705@eagle.fgcu.edu>
2016-06-20 17:47:52 +0000

    app

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var moment = require("moment")
-var fs = require("fs");
 var path = require("path");
 var api = require("./app.js");
 var app = express();
@@ -23,32 +22,19 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/:query", function(req, res) {
-    var date = req.params.query;
-    var unix;
-    var natty;
-    var dateJSON = { "unix": unix, "natural": natty }
-        
-    if (+date === parseInt(date, 10)) {
-        unix = +date;
-        natty = unixToNatty(unix);
-    }
-    else if (moment(date, "MMMM D, YYYY").isValid()) {
-        natty = date;
-        unix = nattyToUnix(natty);
+app.get("/:date", function(req, res) {
+    var date = req.params.date;
+    
+    if (date.isValid()) {
+        res.json({
+            "unix": moment(date, "X"),
+            "natural": moment(date, "MMMM D, YYYY")
+        });
     }
     else {
-        unix = null;
-        natty = null;
+        res.json({
+            "unix": null,
+            "natural": null
+        });        
     }
-        
-    res.json(dateJSON);
 });
-    
-function unixToNatty(unix) {
-    return moment.unix(unix).format("MMMM D, YYYY");
-}
-    
-function nattyToUnix(natty) {
-    return natty.unix();
-}
